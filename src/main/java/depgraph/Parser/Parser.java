84044c34054d34d8a9d32a4abcd7153286545295
spoofaloof <44884605+spoofaloof@users.noreply.github.com>
2020-10-28 21:29:12 -0400

    updated the id name for future purposes
    id is now unique for nodes

--- src/main/java/depgraph/Parser/Parser.java
+++ src/main/java/depgraph/Parser/Parser.java
@@ -46,11 +46,13 @@ public class Parser {
 	/**
 	 * No-arg constructor.
 	 */
+    private int lastNodeId;
 	public Parser() {
         lexer = new Lexer();
         nodes = new ArrayList<Node>();
         edges = new ArrayList<Edge>();
         modules = new ArrayList<Module>();
+        lastNodeId = 0;
 	}
 
 	/**
@@ -186,7 +188,8 @@ public class Parser {
 				Node tempDstNode = getGlobalNodeFromNodeLabel(edge.getDestinationNodeObject().getNodeLabel());
 				edge.setDestinationNodeId(tempDstNode.getNodeId());
 				edge.setDestinationNodeObject(tempDstNode);
-			}
+            }
+
 			newCollection.add(edge);
 		}
 
@@ -216,8 +219,11 @@ public class Parser {
 	private ArrayList<Node> cleanUpNodeCollection(ArrayList<Node> oldCollection) {
 		ArrayList<Node> newCollection = new ArrayList<Node>();
 		for (Node node : oldCollection)
-			if (!isDuplicate(node.getNodeLabel()))
-				newCollection.add(node);
+            if (!isDuplicate(node.getNodeLabel())){
+                node.setNodeId("Node"+this.lastNodeId);
+                lastNodeId++;
+                newCollection.add(node);
+            }
 		return newCollection;
 	}
 

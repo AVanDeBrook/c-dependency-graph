774df2d74b68d823b8bb46cf7155a5e8c656c692
Corrina Del Greco <corrinadelgreco@gmail.com>
2020-11-12 14:08:49 -0500

    beef up the tests for the reader

--- src/test/java/depgraph/TestReader.java
+++ src/test/java/depgraph/TestReader.java
@@ -1,13 +1,48 @@
 package depgraph;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 import depgraph.Reader.Reader;
 
 public class TestReader {
+
+	@Test
+	public void testReadSingleFile() {
+		Reader reader = new Reader();
+		List<String> filesList = null;
+		try {
+			filesList = reader.readSingleFile("test/dot-files/adc_8c_ae0b9ae6e4ef2dbf771dcc0ea30901ae2_cgraph.dot");
+		} catch (Exception e) {
+			assertTrue(false);
+		}
+		assertNotNull(filesList);
+		assertEquals(filesList.size(), 1);
+		assertNotNull(filesList.get(0));
+	}
+
+	@Test
+	public void testReadDirectory() {
+		Reader reader = new Reader();
+		List<String> filesList = null;
+		try {
+			filesList = reader.readDirectory("test/dot-files");
+		} catch (Exception e) {
+			assertTrue(false);
+		}
+		assertNotNull(filesList);
+		assertTrue(filesList.size() > 0);
+		for (String file : filesList) {
+			assertNotNull(file);
+		}
+	}
 
 	@Test
 	public void testReadSingleFileDoesNotThrowException() {
